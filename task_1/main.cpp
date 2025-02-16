80298d3b87689e1c07fb70239740c1661aed12f1
Dark L1ght <georgmaximov160805@gmail.com>
2025-02-17 02:42:26 +0700

    task 1

---
+++ task_1/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string.h>
+
+#define USE_DOUBLE
+
+template <typename T>
+void count_sum(T* sum){
+    float pi = 3.1415926535;
+    T step = 2 * pi / 1e7;
+    std::vector<T> vec(1e7);
+
+    for(int i = 0; i < vec.size(); i++){
+        vec[i] = sin(step * i);
+        *sum += vec[i];
+    }
+}
+
+
+int main(int argc, const char** argv){
+    #ifdef USE_DOUBLE
+        std::cout << "Using double\n";
+        double sum = 0;
+    #endif
+    #ifndef USE_DOUBLE
+        std::cout << "Using float\n";
+        float sum = 0;
+    #endif
+
+    count_sum(&sum);
+    std::cout << "Sum = " << sum << std::endl;
+    return 0;
+}
