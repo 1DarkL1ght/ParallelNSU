927e50fc13880911fe4c3e71e5ea8bad2e2abac9
Maksimov <georgmaximov160805@gmail.com>
2025-02-28 00:39:18 +0700

    task_2 without python

---
+++ task_2/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+void matrix_vector_product(double *a, double *b, double *c, int m, int n, int ncores){
+    #pragma omp parallel num_threads(ncores)
+    {
+        int nthreads = omp_get_num_threads();
+        int threadid = omp_get_thread_num();
+        int items_per_thread = m / nthreads;
+        
+        int lb = threadid * items_per_thread;
+        int ub = (threadid == nthreads - 1) ? (m - 1) : (lb + items_per_thread - 1);
+
+        for (int i = 0; i < m; i++) {
+            c[i] = 0.0;
+            for (int j = 0; j < n; j++)
+                c[i] += a[i * n + j] * b[j];
+        }
+    }
+}
+
+void run_parallel(int m, int n, int ncores){
+    double *a, *b, *c;
+
+    a = (double*)malloc(sizeof(*a) * m * n);
+    b = (double*)malloc(sizeof(*b) * n);
+    c = (double*)malloc(sizeof(*c) * m);
+
+    #pragma omp parallel num_threads(ncores)
+    {
+        int nthreads = omp_get_num_threads();
+        int threadid = omp_get_thread_num();
+        int items_per_thread = m / nthreads;
+        int lb = threadid * items_per_thread;
+        int ub = (threadid == nthreads - 1) ? (m - 1) : (lb + items_per_thread - 1);
+        for (int i = lb; i <= ub; i++) {
+            for (int j = 0; j < n; j++)
+                a[i * n + j] = i + j;
+            c[i] = 0.0;
+        }
+    }
+    for (int j = 0; j < n; j++)
+        b[j] = j;
+
+    double t = omp_get_wtime();
+    matrix_vector_product(a, b, c, m, n, ncores);
+    t = omp_get_wtime() - t;
+
+    printf("Elapsed time (parallel): %.6f sec.\n", t);
+    free(a);
+    free(b);
+    free(c);
+    
+}
+
+int main(){
+    int ncores = 1;
+    int m = 20000, n = 20000;
+
+    printf("Matrix-vector product (c[m] = a[m, n] * b[n]; m = %d, n = %d, n_cores = %d)\n", m, n, ncores);
+    // printf("Memory used: %" PRIu64 " MiB\n", ((m * n + m + n) * sizeof(double)) >> 20);
+
+    run_parallel(m, n, ncores);
+
+    return 0;
+}
