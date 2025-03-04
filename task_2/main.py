8847104bdd0e984df279d32dd162c47ce9850b9e
1DarkL1ght <georgmaximov160805@gmail.com>
2025-03-04 18:03:40 +0000

    doing task_2

--- task_2/main.py
+++ task_2/main.py
@@ -1,48 +1,61 @@
 import subprocess
 import csv
 
+import matplotlib.pyplot as plt
+
 def main():
     threads_arr = [1, 2, 4, 7, 8, 16, 20, 40]
     sizes_arr = [20000, 40000]
     threads = dict(zip(threads_arr, range(len(threads_arr))))
     sizes = dict(zip(sizes_arr, range(len(sizes_arr))))
-    outputs = [[0] * len(threads_arr)] * len(sizes)
-    speedups = [[0] * (len(threads_arr) - 1)] * len(sizes)
+    outputs = [[0 for _ in range(len(threads_arr))] for _ in range(len(sizes))]
+    speedups = [[0 for _ in range(len(threads_arr))] for _ in range(len(sizes))]
+    final_data = [[0 for _ in range(len(threads_arr) * 2 - 1)] for _ in range(len(sizes))]
 
-    final_data = [[0] * (len(threads_arr) * 2 - 1)] * len(sizes)
 
     for num_threads in threads_arr:
         for size in sizes_arr:
-            output = str(subprocess.run(f"main.exe {num_threads} {size}", 
+            output = str(subprocess.run(f"./main {num_threads} {size}", 
                 capture_output=True, shell=True, text=True, encoding='cp866').stdout)
             
-            print(f"nthreads = {num_threads}, size = {size}, output = {output}")
             outputs[sizes[size]][threads[num_threads]] = output
+            print(f"nthreads = {num_threads}, size = {size}, output = {output}")
 
-    for num_threads in threads_arr[1:]:
+    for num_threads in threads_arr:
         for size in sizes_arr:
-            speedups[sizes[size]][threads[num_threads]] = float(outputs[sizes[size]][threads[num_threads]]) / float(outputs[sizes[size]][threads[1]])
+            speedups[sizes[size]][threads[num_threads]] = float(outputs[sizes[size]][threads[1]]) / float(outputs[sizes[size]][threads[num_threads]])
 
 
-
-    # df = pd.DataFrame(outputs, index=[20000, 40000], columns=threads_arr)
-    # print(df.head())
+    print(f"test = {outputs[sizes[20000]][threads[1]]}, {outputs[sizes[40000]][threads[1]]}")
 
     with open('threads_times.csv', 'w') as csvfile:
         writer = csv.writer(csvfile, dialect='excel')
 
         writer.writerow(["", "T1", "T2", "S2", "T4", "S4", "T7", "S7", "T8", "S8", "T16", "S16", "T20", "S20", "T40", "S40"])
         
-        for size in sizes_arr:
-            writer.writerow([size, 
-                outputs[sizes[size]][threads[1]], 
-                outputs[sizes[size]][threads[2]], speedups[sizes[size]][0], 
-                outputs[sizes[size]][threads[4]], speedups[sizes[size]][1], 
-                outputs[sizes[size]][threads[7]], speedups[sizes[size]][2], 
-                outputs[sizes[size]][threads[8]], speedups[sizes[size]][3],
-                outputs[sizes[size]][threads[16]], speedups[sizes[size]][4],
-                outputs[sizes[size]][threads[20]], speedups[sizes[size]][5],
-                outputs[sizes[size]][threads[40]], speedups[sizes[size]][6]
-            ])
+        writer.writerow([20000, 
+            outputs[sizes[20000]][threads[1]], 
+            outputs[sizes[20000]][threads[2]], speedups[sizes[20000]][1], 
+            outputs[sizes[20000]][threads[4]], speedups[sizes[20000]][2], 
+            outputs[sizes[20000]][threads[7]], speedups[sizes[20000]][3], 
+            outputs[sizes[20000]][threads[8]], speedups[sizes[20000]][4],
+            outputs[sizes[20000]][threads[16]], speedups[sizes[20000]][5],
+            outputs[sizes[20000]][threads[20]], speedups[sizes[20000]][6],
+            outputs[sizes[20000]][threads[40]], speedups[sizes[20000]][7]
+        ])
+
+        writer.writerow([40000, 
+            outputs[sizes[40000]][threads[1]], 
+            outputs[sizes[40000]][threads[2]], speedups[sizes[40000]][1], 
+            outputs[sizes[40000]][threads[4]], speedups[sizes[40000]][2], 
+            outputs[sizes[40000]][threads[7]], speedups[sizes[40000]][3], 
+            outputs[sizes[40000]][threads[8]], speedups[sizes[40000]][4],
+            outputs[sizes[40000]][threads[16]], speedups[sizes[40000]][5],
+            outputs[sizes[40000]][threads[20]], speedups[sizes[40000]][6],
+            outputs[sizes[40000]][threads[40]], speedups[sizes[40000]][7]
+        ])
+
+    fig, ax = plt.subplots(1, 1)
+    
 
 main()
---
+++ task_2/testc.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <omp.h>
+
+int main(){
+    #pragma omp parallel
+    {
+        std::cout << "Thread " << omp_get_thread_num() << " of " << omp_get_num_threads() << std::endl;
+    }
+
+    return 0;
+}
