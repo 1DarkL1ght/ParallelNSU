16365b9f702993bfbd4ebe4d3bfd8f50af502d36
Dark L1ght <georgmaximov160805@gmail.com>
2025-03-21 15:52:41 +0700

    variant2 measured

--- lab_2/task_3/make_plot.py
+++ lab_2/task_3/make_plot.py
@@ -7,27 +7,29 @@ def main(type):
     times = []
     threads_arr = [1, 2, 4, 7, 8, 16, 20, 40]
 
-    with open(f'threads_times_{type}.csv', 'r') as csvfile:
+    with open(f'threads_times_3_{type}.csv', 'r') as csvfile:
         csv_reader = csv.reader(csvfile)
+        
         for row in csv_reader:
-            for i in range(2, len(row), 2):
-                speedups.append(float(row[i]))
+            if row[0] != 'T1':
+                for i in range(2, len(row), 2):
+                    speedups.append(float(row[i]))
 
-            times.append(float(row[0]))
-            for i in range(1, len(row), 2):
-                times.append(float(row[i]))
+                times.append(float(row[0]))
+                for i in range(1, len(row), 2):
+                    times.append(float(row[i]))
 
             
 
 
-    fig, (ax1, ax2) = plt.subplots(1, 1)
+    fig, (ax1, ax2) = plt.subplots(2, 1)
 
     ax1.plot(threads_arr[1:], speedups, color='green', label='M=20000', marker='o')
     ax1.plot(threads_arr[1:], threads_arr[1:], color='blue', linestyle='dashed', label='Linear', marker='o')  
     ax1.grid()
     ax1.legend()
 
-    ax2.plot(threads_arr[1:], times, color = 'green', label='Variant2', marker='0')
+    ax2.plot(threads_arr, times, color = 'green', label='Variant2', marker='o')
     ax2.grid()
     ax2.legend()
     fig.savefig(f"speedups{type}.png")
